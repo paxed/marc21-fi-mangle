e418a0f09cf64cf53ed0fe121f43e5cb07366473
Pasi Kallinen <pasi.kallinen@pttk.fi>
2014-12-09 11:41:18 +0200

    Make marc_warnings easier to config

--- marc_warnings.pl
+++ marc_warnings.pl
@@ -10,11 +10,16 @@ use DBI;
 use Data::Dumper;
 use XML::TreePP;
 
-use constant EG_DBNAME => "evergreen";
-use constant EG_HOST => "localhost";
-use constant EG_USER => "evergreen";
-use constant EG_PASS => "evergreen";
-use constant EG_MARC_XML => '/root/eg/Evergreen/Open-ILS/xul/staff_client/server/locale/en-US/marcedit-tooltips.xml';
+
+use constant DB_HOSTNAME => 'localhost';
+use constant DB_USERNAME => 'kohaadmin';
+use constant DB_PASSWORD => 'katikoan';
+use constant DB_DBNAME   => 'koha';
+use constant DB_DRIVER   => 'mysql';
+
+use constant MARC_XML => './marcedit-tooltips.xml';
+#use constant DB_QUERY => 'select id, marc from biblio.record_entry order by id asc';
+use constant DB_QUERY => 'select biblionumber as id, marcxml as marc from biblioitems order by id asc';
 
 my %not_repeatable;
 
@@ -22,7 +27,7 @@ my %not_repeatable;
 
 my $tpp = XML::TreePP->new();
 $tpp->set( utf8_flag => 1 );
-my $tree = $tpp->parsefile(EG_MARC_XML);
+my $tree = $tpp->parsefile(MARC_XML);
 
 my @treefields = $tree->{'fields'}{'field'};
 
@@ -88,7 +93,7 @@ sub check_marc {
 }
 
 sub db_connect {
-    my $dbh = DBI->connect("DBI:Pg:dbname=" . EG_DBNAME . ";host=" . EG_HOST, EG_USER, EG_PASS, {'RaiseError' => 1});
+    my $dbh = DBI->connect("DBI:" . DB_DRIVER . ":dbname=" . DB_DBNAME . ";host=" . DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, {'RaiseError' => 1});
     if (!$dbh) {
 	print "DB Error.";
 	footer();
@@ -106,7 +111,7 @@ my $sql = "";
 my $sth;
 my $dbh = db_connect();
 
-$sql = "select id, marc from biblio.record_entry order by id asc";
+$sql = DB_QUERY;
 $sth = $dbh->prepare($sql);
 
 $sth->execute();
