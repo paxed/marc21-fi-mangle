df6341ca0e4673134a593b4fc0d244104e469c6d
Pasi Kallinen <pasi.kallinen@koha-suomi.fi>
2019-01-29 09:34:10 +0200

    update frameworks shell script

---
+++ update_frameworks.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+#10 03 * * *        cd /home/koha/marc21-fi-mangle && ./luo_virhelista.sh
+#02 03 1 * *        cd /home/koha/marc21-fi-mangle && ./get_fi_xmls.sh && ./update_frameworks.sh
+
+TUNNUS=VAARA
+MAILI_TO=myemail@example.com
+MAILI_FROM=fromemail@example.com
+MAILI_SUBJ="Luettelointipohjien muutokset"
+MAILI_TEXT=""
+
+PVM=$(date +%Y%m%d)
+FILEA=/tmp/upsert_${TUNNUS}_default.log
+FILEB=/tmp/upsert_${TUNNUS}_kaikki.log
+FILEC=/tmp/${TUNNUS}_marc21_framework_updates_${PVM}.log
+FILEE=/tmp/upsert_${TUNNUS}_errors.log
+DBPARAMS="-db username=root -db password='' -db dbname=koha -db hostname=127.0.0.1"
+
+if [ ! -e "$FILEC" ]; then
+    # default framework
+    perl upsert_marc_fields.pl $DBPARAMS --flags=intranet,opac,editor --insert --update > "$FILEA" 2> "$FILEE"
+
+    # all other frameworks
+    perl upsert_marc_fields.pl $DBPARAMS --framework='*-' --flags=intranet,opac --insert --update > "$FILEB" 2>> "$FILEE"
+
+    cat "$FILEE" > "$FILEC"
+    cat "$FILEA" >> "$FILEC"
+    sort "$FILEB" >> "$FILEC"
+fi
+
+RIVIT=$(wc -l "$FILEC" | cut -d' ' -f 1)
+
+if test $RIVIT -gt 0
+then
+    echo "${MAILI_TEXT}" | mail -s "${MAILI_SUBJ}" -a "$FILEC" -r "${MAILI_FROM}" "${MAILI_TO}"
+fi
