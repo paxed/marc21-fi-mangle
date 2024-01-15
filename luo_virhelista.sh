9d66eec230e80a35bda3f3dd8947215b3156087f
Pasi Kallinen <pasi.kallinen@koha-suomi.fi>
2024-01-15 06:50:46 +0200

    Get ignored fields from Nalkutin plugin data

--- luo_virhelista.sh
+++ luo_virhelista.sh
@@ -3,12 +3,24 @@
 test -z "$KOHA_CONF" && echo "No KOHA_CONF." && exit 1
 getconf() { xmllint --xpath "yazgfs/config/$1/text()" $KOHA_CONF 2> /dev/null; }
 DBNAME="$(getconf database)"
+HOSTNAME="$(getconf hostname)"
+DBPORT="$(getconf port)"
+DBUSER="$(getconf user)"
+DBPASS="$(getconf pass)"
+
 OUTPATH=/var/koha
+
+IGNORES=$(mysql -h $HOSTNAME -P $DBPORT -u $DBUSER -p$DBPASS --protocol=tcp $DBNAME -NBe "select plugin_value from plugin_data where plugin_class like '%::Nalkutin' and plugin_key = 'ignore_marc_fields';")
+
+if [ "x$IGNORES" = "x" ]; then
+  IGNORES="59x,790,8845,9xx,xxx9"
+fi
+
 
 perl marc_warnings.pl \
     -sql='select biblionumber as id, metadata as marc, ExtractValue(metadata, "//controlfield[@tag=003]") as f003, ExtractValue(metadata, "//datafield[@tag=040]/subfield[@code=\"a\"]") as f040a, ExtractValue(metadata, "//datafield[@tag=040]/subfield[@code=\"d\"]") as f040d, CONCAT(ExtractValue(metadata, "//controlfield[@tag=003]")," ",ExtractValue(metadata, "//datafield[@tag=040]/subfield[@code=\"a\"]")," ",ExtractValue(metadata, "//datafield[@tag=040]/subfield[@code=\"d\"]")," ",biblionumber) as urllink from biblio_metadata order by f003, f040a, f040d, biblionumber asc' \
     -biburl='/cgi-bin/koha/catalogue/MARCdetail.pl?biblionumber=%s' \
-    -ignore=59x,790,9xx,xxx9 -skip-enclevels=78 \
+    -ignore="$IGNORES" -skip-enclevels=78 \
     > "$OUTPATH/marc_virheet.$DBNAME.html"
 
 grep -a '<li>' "$OUTPATH/marc_virheet.$DBNAME.html" \
